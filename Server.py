449a08ca3f068ff26dc6d2e238eab6f4cf8321c7
Xin Pang <pangxin0803@gmail.com>
2023-01-05 23:07:52 +0100

    first commit

--- Server.py
+++ Server.py
@@ -1,26 +1,43 @@
 import socket
-# Create a UDP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Server application IP address and port
-server_address = '127.0.0.1'
-server_port = 10001
 
-# Buffer size
-buffer_size = 1024
+def broadcast(ip, port, broadcast_message):
+    # Create a UDP socket
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Send message on broadcast address
+    broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
+    broadcast_socket.close()
 
-message = 'Hi client! Nice to connect with you!'
 
-# Bind socket to port
-server_socket.bind((server_address, server_port))
-print('Server up and running at {}:{}'.format(server_address, server_port))
-while True:
-    print('\nWaiting to receive message...\n')
+class Server:
+    def __init__(self, server_socket):
+        self.server_socket = server_socket
+        self.isFollower = True
+        self.isCandidate = False
+        self.isLeader = False
 
-    data, address = server_socket.recvfrom(buffer_size)
-    print('Received message from client: ', address)
-    print('Message: ', data.decode())
 
-    if data:
-        server_socket.sendto(str.encode(message), address)
-        print('Replied to client: ', message)
+if __name__ == '__main__':
+    # create a UDP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Server application IP address and port
+    MY_HOST = socket.gethostname()
+    server_address = socket.gethostbyname(MY_HOST)
+    server_port = -1
+
+    # Buffer size
+    buffer_size = 1024
+
+    # Bind server socket:
+    server_socket.bind((server_address, 0))
+    server_port = server_socket.getsockname()[1]
+    broadcast_address = server_address
+    server = Server(server_socket)
+    message = 'Hi client! This is server at {}:{}'.format(server_address, server_port)
+    print(message)
+
+
+
+    while True:
+        data, address = server_socket.recvfrom(buffer_size)
---
+++ broadcastsender.py
@@ -0,0 +1,30 @@
+import socket
+
+
+def broadcast(ip, port, broadcast_message):
+    # Create a UDP socket
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Send message on broadcast address
+    broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
+    print('broadcast message sent.')
+    broadcast_socket.close()
+
+
+if __name__ == '__main__':
+
+    # Local host information
+    MY_HOST = socket.gethostname()
+    MY_IP = socket.gethostbyname(MY_HOST)
+
+    # Broadcast address and port
+    BROADCAST_IP = MY_IP[:]
+    BROADCAST_IP = BROADCAST_IP[:-2] + '255'
+    print('ip:{}, broadcast address:{}'.format(MY_IP, BROADCAST_IP))
+    BROADCAST_PORT = 64922
+
+    # Send broadcast message
+    message = MY_IP + ' sent a broadcast'
+    broadcast(BROADCAST_IP, BROADCAST_PORT, message)
+
