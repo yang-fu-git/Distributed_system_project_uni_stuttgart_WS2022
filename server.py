230738229136453f83b533e1feb5c908acaad07f
helena615 <ning980615@gmail.com>
2023-01-05 23:28:11 +0100

    merge

--- server.py
+++ server.py
@@ -1,48 +1,43 @@
-import multiprocessing
 import socket
-import os
 
 
-class Server(multiprocessing.Process):
+def broadcast(ip, port, broadcast_message):
+    # Create a UDP socket
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Send message on broadcast address
+    broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
+    broadcast_socket.close()
+
+
+class Server:
     def __init__(self, server_socket):
-        super(Server, self).__init__()
         self.server_socket = server_socket
-        self.client_address = None
-        self.received_data = None
+        self.isFollower = True
+        self.isCandidate = False
+        self.isLeader = False
 
 
-    def run(self):
-        message = 'This is server ' + str(os.getpid())
-        
-        print(message)
-
-        # Buffer size
-        buffer_size = 1024
-
-        while True: 
-            data, address = self.server_socket.recvfrom(buffer_size)
-            
-            self.received_data = data
-            self.client_address = address
-
-            self.server_socket.sendto(str.encode(message), self.client_address)
-            print('Server: {} received message \'{}\' at {}:{}'.format(str(os.getpid()), data.decode(), address[0], address[1]))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # create a UDP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Bind the socket to the port
-    server_address = '127.0.0.1'
-    server_port = 10001
+    # Server application IP address and port
+    MY_HOST = socket.gethostname()
+    server_address = socket.gethostbyname(MY_HOST)
+    server_port = -1
 
-    
+    # Buffer size
+    buffer_size = 1024
 
-    server_socket.bind((server_address, server_port))
-    print('Server up and running at {}:{}'.format(server_address, server_port))
+    # Bind server socket:
+    server_socket.bind((server_address, 0))
+    server_port = server_socket.getsockname()[1]
+    broadcast_address = server_address
+    server = Server(server_socket)
+    message = 'Hi client! This is server at {}:{}'.format(server_address, server_port)
+    print(message)
 
-    # while(True):
-    for i in range(3):
-        
-        p = Server(server_socket)
-        p.start()
-    # p.join()
+
+
+    while True:
+        data, address = server_socket.recvfrom(buffer_size)
