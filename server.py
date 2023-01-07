d124e41f0e23318ddc2e9ac3b672e98033432719
helena615 <ning980615@gmail.com>
2023-01-07 17:01:47 +0100

    fix to: server repeatition

--- server.py
+++ server.py
@@ -1,28 +1,13 @@
 import socket
-import time
-from threading import Thread
+
 
 def broadcast(ip, port, broadcast_message):
     # Create a UDP socket
     broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
-    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Send message on broadcast address
     broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
-    print('broadcast message sent.')
     broadcast_socket.close()
 
-
-def getBroadcastIP(MY_IP):
-    ip = MY_IP.split('.')
-    BROADCAST_IP = ip[0] + '.' + ip[1] + '.' + ip[2] + '.255'
-    return BROADCAST_IP
-
-def sendHeartbeat(bIP, port, mIP):
-    msg = 'Alive: ' + mIP
-    while True:
-        broadcast(bIP, port, msg)
-        time.sleep(1)
 
 class Server:
     def __init__(self, server_socket):
@@ -39,31 +24,12 @@ if __name__ == '__main__':
     # Server application IP address and port
     MY_HOST = socket.gethostname()
     server_address = socket.gethostbyname(MY_HOST)
-    # server_port = -1
-    server_port = 64922
+    server_port = -1
 
     # Buffer size
     buffer_size = 1024
-    
-    # Broadcast address and port
-    BROADCAST_IP = getBroadcastIP(server_address)
 
-    # Send broadcast message
-    sendHeartbeatMsg = Thread(target=sendHeartbeat, args = (BROADCAST_IP, server_port, server_address))
-    sendHeartbeatMsg.start()
-
-"""    # Broadcast address and port
-    BROADCAST_IP = MY_HOST[:]
-    BROADCAST_IP = BROADCAST_IP[:-2] + '255'
-    print('ip:{}, broadcast address:{}'.format(MY_HOST, BROADCAST_IP))
-    BROADCAST_PORT = 64922"""
-
-    
-"""    message = MY_HOST + ' sent a broadcast'
-    broadcast(BROADCAST_IP, BROADCAST_PORT, message)"""
-
-
-"""    # Bind server socket:
+    # Bind server socket:
     server_socket.bind((server_address, 0))
     server_port = server_socket.getsockname()[1]
     broadcast_address = server_address
@@ -74,4 +40,4 @@ if __name__ == '__main__':
 
 
     while True:
-        data, address = server_socket.recvfrom(buffer_size)"""
+        data, address = server_socket.recvfrom(buffer_size)
