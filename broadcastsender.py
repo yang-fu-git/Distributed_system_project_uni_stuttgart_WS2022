c7e338c8add7d01786252757099639ea80053491
Xin Pang <pangxin0803@gmail.com>
2023-01-31 19:28:48 +0100

    save

--- broadcastsender.py
+++ broadcastsender.py
@@ -5,7 +5,7 @@ def broadcast(ip, port, broadcast_message):
     # Create a UDP socket
     broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
-    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Send message on broadcast address
     broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
     print('broadcast message sent.')
@@ -19,12 +19,13 @@ if __name__ == '__main__':
     MY_IP = socket.gethostbyname(MY_HOST)
 
     # Broadcast address and port
-    BROADCAST_IP = MY_IP[:]
-    BROADCAST_IP = BROADCAST_IP[:-2] + '255'
+    # BROADCAST_IP = MY_IP[:]
+    BROADCAST_IP = '192.168.178.255'
     print('ip:{}, broadcast address:{}'.format(MY_IP, BROADCAST_IP))
     BROADCAST_PORT = 64922
 
     # Send broadcast message
     message = MY_IP + ' sent a broadcast'
     broadcast(BROADCAST_IP, BROADCAST_PORT, message)
+
 
